16e4f84d9494361f001d2234b98c5c46c2932806
Abdul Rahman <abdulnsamba@gmail.com>
2025-01-17 12:03:51 +0300

    added quiz files

---
+++ quiz.js
@@ -0,0 +1,16 @@
+const feedback = document.querySelector("#feedback");
+const btn = document.getElementById("submit-answer");
+
+function checkAnswer() {
+  const choice = document.querySelector("input[name]:checked");
+  let correctAnswer = "4";
+  let userAnswer = choice.value;
+
+  if (userAnswer === correctAnswer) {
+    feedback.textContent = "Correct! Well done.";
+  } else {
+    feedback.textContent = "That's incorrect. Try again!";
+  }
+}
+
+btn.addEventListener("click", checkAnswer);
